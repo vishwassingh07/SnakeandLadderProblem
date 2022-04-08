9d29336f493638efc3f231b722127dfba64aa796
Vishwas <baghel.vishwas0@gmail.com>
2022-04-08 13:22:33 +0530

    [vishwas]add: Checking For an Option In No Play, Snake and Ladder

--- SnakeandLadderProblem/Program.cs
+++ SnakeandLadderProblem/Program.cs
@@ -1,3 +1,13 @@
-﻿using SnakeandLadderProblem;
-SnakeandLadder game1 = new SnakeandLadder();
-Console.WriteLine(game1.DieRoll());
+﻿using System;
+namespace SnakeandLadderProblem
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            SnakeandLadder game = new SnakeandLadder();
+            Console.WriteLine(game.DieRoll());
+
+        }
+    }
+}
--- SnakeandLadderProblem/SnakeandLadder.cs
+++ SnakeandLadderProblem/SnakeandLadder.cs
@@ -8,12 +8,27 @@ namespace SnakeandLadderProblem
 {
     public class SnakeandLadder
     {
+        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
         int position = 0;
         public int DieRoll()
         {
             Random random = new Random();
             int diePosition = random.Next(1, 7);
             return diePosition;
+        }
+        public void Game()
+        {
+            Random random = new Random();
+            int option = random.Next(0,3);
+            switch (option)
+            {
+                case NO_PLAY:
+                    break;
+                case SNAKE:this.position -= this.DieRoll();
+                    break;
+                case LADDER:this.position += this.DieRoll();
+                    break;
+            }
         }
     }
 }
